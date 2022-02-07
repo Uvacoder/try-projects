e28634a928435c3fedcabdd825a54ceb475f8756
Vasanth Developer <vsnthdev@gmail.com>
2022-02-07 20:06:20 +0530

    ✨ designed alpa-dash

---
+++ src/pages/alpa-dash/index.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const createBtn = document.querySelector('#create')
+    const loader = createBtn.querySelector('svg')
+    const text = createBtn.querySelector('span')
+
+    const input = document.querySelector('#quickCreate')
+
+    const onSubmit = () => {
+        if (Boolean(input.value) == false) return
+
+        loader.classList.remove('opacity-0')
+        text.classList.add('opacity-0')
+
+        setTimeout(() => {
+            loader.classList.add('opacity-0')
+            text.classList.remove('opacity-0')
+        }, 1000)
+
+        console.log('Create a new short link.')
+    }
+
+    input.addEventListener('keyup', e => {
+        if (e.keyCode == 13) {
+            if (Boolean(e.target.value)) onSubmit()
+        } else {
+            if (Boolean(e.target.value)) {
+                text.textContent = 'Quick create'
+            } else {
+                text.textContent = 'Create'
+            }
+        }
+    })
+
+    createBtn.addEventListener('click', onSubmit)
+})
--- src/pages/alpa-login/tailwind.web.js
+++ src/pages/alpa-login/tailwind.web.js
@@ -2,6 +2,7 @@ tailwind.config = {
     theme: {
         fontFamily: {
             body: ['Readex Pro', 'sans-serif'],
+            mono: ['Red Hat Mono', 'monospace'],
         },
         extend: {
             colors: {},
