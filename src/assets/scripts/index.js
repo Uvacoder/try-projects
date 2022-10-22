086be5fe56139c6bf3c10862063a6c2ea474b05a
Vasanth Developer <vsnthdev@gmail.com>
2022-10-23 03:00:18 +0530

    🚧 using StackBlitz

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,74 +1,39 @@
-import './tailwind.web.js'
+// https://stackblitz.com/edit/web-platform-jaa2ij?embed=1&file=index.html&hideExplorer=1&hideNavigation=1
 
-import axios from 'https://cdn.skypack.dev/axios'
-import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
+const btnClicked = e => {
+    const stackBlitz = e.target.dataset.stackblitz
 
-// unregister all service workers
-const workers = await navigator.serviceWorker.getRegistrations()
-for (const worker of workers) worker.unregister()
-
-// add the transition class after some time
-setTimeout(() => {
-    document.querySelector('body').classList.add('transition-colors')
-}, 500)
-
-const main = async () => {
-    const octokit = new Octokit()
-
-    const { data } = await octokit.rest.repos.getContent({
-        owner: 'vsnthdev',
-        repo: 'try',
-        path: '/src/pages',
-    })
-
-    const hidden = ['index']
-
-    const markup = ({ bg, title, category, url }) => `<a href="/pages${url}">
-    <article class="relative bg-center bg-cover bg-no-repeat rounded-xl shadow-card p-8 flex flex-col justify-between my-4 w-80 h-108 transition focus:outline-none active:transform-gpu active:scale-95 lg:my-12 lg:w-full lg:flex-row lg:items-center lg:h-auto lg:py-10" style="background: #${bg}">
-        <div class="flex-grow-0 text-white z-10">
-            <span class="text-sm font-black uppercase tracking-widest opacity-30">${category}</span>
-            <h3 class="text-3xl font-bold mt-2">${title}</h3>
-        </div>
-        <div class="flex-shrink-0 lg:mt-auto z-10">
-            <button class="text-light-900 shadow-card bg-white transition hover:bg-gray-200 font-semibold px-4 py-2 rounded-md focus:outline-none">Explore now</button>
-        </div>
-    </article>
-    </a>`
-
-    const safetyCheck = (elm, field) => {
-        if (elm) {
-            return elm.getAttribute('content')
-        } else {
-            return `No ${field}`
-        }
-    }
-
-    for (const folder of data) {
-        if (hidden.includes(folder.name)) continue
-        if (folder.type == 'file') continue
-
-        const { data: contents } = await axios({
-            method: 'GET',
-            url: `https://raw.githubusercontent.com/vsnthdev/try/main/src/pages/${folder.name}/index.html`,
-        })
-
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(contents, 'text/html')
-        const cards = document.querySelector('#cards')
-
-        const title = safetyCheck(doc.querySelector('meta[name=vsnthdev-try-title]'), 'Title')
-        const category = safetyCheck(doc.querySelector('meta[name=vsnthdev-try-category]'), 'Category')
-        const bg = safetyCheck(doc.querySelector('meta[name=vsnthdev-try-bg]'), 'Background')
-
-        cards.innerHTML =
-            cards.innerHTML +
-            markup({
-                bg,
-                title,
-                category,
-                url: `/${folder.name}`,
-            })
+    if (stackBlitz) {
+        const iframe = document.querySelector('iframe')
+        const container = iframe.parentElement
+        
+        container.classList.remove('pointer-events-none', 'opacity-0')
     }
 }
 
-main()
+window.addEventListener('DOMContentLoaded', () => {
+    const btns = Array.from(document.querySelectorAll('article button'))
+    btns.forEach(btn => btn.addEventListener('click', btnClicked))
+
+    document.querySelector('iframe').src = `https://stackblitz.com/edit/${btns[0].dataset.stackblitz}?embed=1&hideExplorer=1&hideNavigation=1&hideDevTools=1&view=preview`
+
+    document.querySelector('iframe').parentElement.addEventListener('click', e => {
+        e.target.classList.add('opacity-0')
+        e.target.classList.add('pointer-events-none')
+    })
+
+    const articles = Array.from(document.querySelectorAll('article'))
+    articles.forEach(article => article.addEventListener('mouseenter', () => {
+        const btn = article.querySelector('button')
+        const stackBlitz = btn.dataset.stackblitz
+
+        if (document.querySelector('iframe').src.includes(stackBlitz) == false) {
+            document.querySelector('iframe').classList.add('opacity-0')
+            document.querySelector('iframe').src = `https://stackblitz.com/edit/${stackBlitz}?embed=1&hideExplorer=1&hideNavigation=1&hideDevTools=1&view=preview`
+        }
+    }))
+
+    document.querySelector('iframe').onload = e => {
+        e.target.classList.remove('opacity-0')
+    }
+})
--- src/assets/scripts/tailwind.web.js
+++ src/assets/scripts/tailwind.web.js
@@ -1,28 +1,10 @@
 /* global tailwind */
 
-import colors from 'https://cdn.skypack.dev/tailwindcss/colors'
-
 tailwind.config = {
     darkMode: 'class',
     theme: {
         fontFamily: {
-            body: ['Basically A Sans Serif', 'sans-serif'],
-        },
-        colors: {
-            light: colors.slate,
-            dark: colors.slate,
-            transparent: 'transparent',
-            inherit: 'inherit',
-            white: '#FFFFFF',
-            brand: '#006cff',
-        },
-        extend: {
-            boxShadow: {
-                card: '0px 14px 15px -3px rgba(0, 0, 0, 0.05)',
-            },
-            spacing: {
-                108: '28rem',
-            },
-        },
-    },
+            sans: ['Basically A Sans Serif', 'sans-serif'],
+        }
+    }
 }
